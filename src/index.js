b1e3ed69b8e9830b1b835cbf9365af44a036d395
MozLee <lixin28@100tal.com>
2019-06-25 14:17:03 +0800

    VER1.0.0

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import {get} from 'lodash'
+// console.log(123123);
+// console.log('ggg');
+// const fetchName = ()=>{
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve({name:'lixin'})
+//         },2000)
+//     });
+
+// }
+// const getName = async() => {
+//     let name = await fetchName()
+//     console.log(name)
+//     console.log(get(name,'name'))
+// }
+// getName()
+import { b } from './lib/test'
+
+b()
---
+++ src/lib/test.js
@@ -0,0 +1,6 @@
+export function a () {
+  console.log(`我是A`)
+}
+export function b () {
+  console.log(`我是B`)
+}
