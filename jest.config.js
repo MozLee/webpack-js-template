e28a4a31a5e86dfaf624afcce55e7d343bc3ea7f
MozLee <lixin@mozlee.com>
2019-08-13 14:50:52 +0800

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  // eslint-disable-next-line no-useless-escape
+  testMatch: ['<rootDir>/src/test/\*\*/\*.js'],
   transform: {
     // 将.js后缀的文件使用babel-jest处理
-    testMatch: ['<rootDir>/src/test/\*\*/\*.js'],
     '^.+\\.js$': 'babel-jest'
   }
 }
